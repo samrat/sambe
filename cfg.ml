0d9a75eceb7ce9c3af0956f2ab6096a121ced38f
Samrat Man Singh <samratmansingh@gmail.com>
2017-04-14 17:15:50 +0545

    Implement unreachable code elimination

--- cfg.ml
+++ cfg.ml
@@ -6,6 +6,9 @@ module Qbe_set = Set.Make (struct
     let compare = compare
   end);;
 
+let dedup xs =
+  Qbe_set.elements (Qbe_set.of_list xs)
+
 
 let func = Lexer.stream_of_string "function w $sum(l %arr, w %num) {
 @start
@@ -22,14 +25,28 @@ let func = Lexer.stream_of_string "function w $sum(l %arr, w %num) {
 	%w =w loadw %idx2
 	%s1 =w add %w, %s0
 	jmp @loop
+@unreachable
+	%idx0 =l extsw %n2
+	%idx1 =l mul 4, %idx0
+	%idx2 =l add %idx1, %arr
+	%w =w loadw %idx2
+	%s1 =w add %w, %s0
+	jmp @loop
 @end
 	ret %s0
 }
 ";;
 (* parse_function func true;; *)
 
-let build_cfg blocks : (qbe, qbe list) Hashtbl.t =
+let build_cfg blocks =
   let graph : (qbe, qbe list) Hashtbl.t = Hashtbl.create 12 in
+  let all_block_labels = List.map (fun block ->
+      match block with
+      | Block(label, _, _, _) -> label
+      | _ -> failwith "function body has zero blocks")
+      blocks
+  in
+  let entry = List.hd all_block_labels in
   let rec build_cfg' blocks =
     match blocks with
     | Block(label, _ , _, Instr1("jmp", dest)) :: rest ->
@@ -38,10 +55,38 @@ let build_cfg blocks : (qbe, qbe list) Hashtbl.t =
     | Block(label, _ , _, Instr3("jnz", _, dest1, dest2)) :: rest ->
       (Hashtbl.add graph label [dest1; dest2]);
       build_cfg' rest
+    | Block(label, _, _, Instr1("ret", _)) :: rest ->
+      build_cfg' rest
     | [] -> graph
     | _ -> failwith "NYI"
   in
-  build_cfg' blocks
+  (entry, all_block_labels, build_cfg' blocks)
+
+
+let find_reachable_nodes (cfg : (qbe, qbe list) Hashtbl.t) (start : qbe) =
+  let rec traverse reachable current =
+    if (List.mem current reachable)
+    then reachable
+    else match Hashtbl.find_option cfg current with
+        | Some(succs) -> List.flatten (List.map (traverse (current::reachable))
+                                         succs)
+        | None -> (current::reachable)
+  in
+  dedup (traverse [] start)
+
+let eliminate_unreachable_blocks fn =
+  let (export, retty, name, params, blocks) = match fn with
+    | FunDef(export, retty, name, params, blocks) -> 
+      (export, retty, name, params, blocks)
+    | _ -> failwith "expected function definition" in
+  let (entry, all_nodes, g) = build_cfg blocks in
+  let reachable_nodes = find_reachable_nodes g entry in
+  let new_blocks = List.filter (fun block ->
+      match block with
+      | Block(label, _, _, _) when List.mem label reachable_nodes ->
+        true
+      | _ -> false) blocks in
+  FunDef(export, retty, name, params, new_blocks)
 
 (* Hashtbl.find_option g (BlockLabel "loop1");; *)
 
@@ -112,7 +157,6 @@ let parsed_func = parse_function func true;;
 let blocks = match parsed_func with
   | FunDef(_, _, _, _, blocks) -> blocks
   | _ -> failwith "expected function definition";;
-let g = build_cfg blocks;;
-let pg = pred_graph g (BlockLabel "start");;
-let all_nodes = [BlockLabel("start"); BlockLabel("loop"); BlockLabel("loop1"); BlockLabel("end")];;
+let (entry, all_nodes, g) = build_cfg blocks;;
+let pg = pred_graph g entry;;
 let doms = dom_solver pg all_nodes (BlockLabel("start"))
