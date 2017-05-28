758021ade62e4d37d3b05fe40b4ed82e68930f15
Samrat Man Singh <samratmansingh@gmail.com>
2017-05-28 22:31:03 +0545

    Tentative `assign_homes` pass(without reg. allocation)

--- cfg.ml
+++ cfg.ml
@@ -222,7 +222,9 @@ let rec instr_to_x86 instr =
         [ IMov(Reg(RAX), get_arg_val arg1);
           ICmp(Const(0), Reg(RAX));
           IJne(get_label arg3);
-          (* ILabel(get_label arg2); *)
+          (* TODO: Can we avoid this jump by generating code for the
+             `arg2` block here? *)
+          IJmp(get_label arg2);
         ]
       | _ -> failwith "NYI"
     end
@@ -232,9 +234,54 @@ let rec instr_to_x86 instr =
 let block_to_x86 block =
   match block with
   | Block(label, [], reg_instrs, last_instr) ->
-    failwith "NYI"
+    [ ILabel(get_label label) ] @
+    List.flatten (List.map instr_to_x86 reg_instrs) @
+    (instr_to_x86 last_instr)
   | Block(_, phis, _, _) -> failwith "expected empty phi instr list"
   | _ -> failwith "NYI"
+
+
+(* TODO: Currently, this puts everything in memory. Implement the
+   register allocation pass to fix that. *)
+let assign_homes (func: instruction list) =
+  let mappings : (string, arg) Hashtbl.t = Hashtbl.create 16 in
+  let counter = ref 0 in
+  let find_loc v =
+    match Hashtbl.find_option mappings v with
+    | Some(loc) -> loc
+    | None ->
+      let new_loc = RegOffset(-8 * !counter, RBP) in
+      Hashtbl.add mappings v new_loc;
+      counter := !counter + 1;
+      new_loc
+  in
+  let replace_instr2_args_with_locs = function
+  | Var(dest), Var(src) ->
+    let dest_loc = find_loc dest in
+    let src_loc = find_loc src in
+    (dest_loc, src_loc)
+  | Var(dest), src ->
+    let dest_loc = find_loc dest in
+    (dest_loc, src)
+  | dest, Var(src) ->
+    let src_loc = find_loc src in
+    (dest, src_loc)
+  | dest, src ->
+    (dest, src)
+  in
+  let fixup_instr = function
+    | IMov(d, s) ->
+      let (new_dest, new_src) = replace_instr2_args_with_locs (d, s) in
+      IMov(new_dest, new_src)
+    | IAdd(d, s) ->
+      let (new_dest, new_src) = replace_instr2_args_with_locs (d, s) in
+      IAdd(new_dest, new_src)
+    | ICmp(d, s) ->
+      let (new_dest, new_src) = replace_instr2_args_with_locs (d, s) in
+      ICmp(new_dest, new_src)
+    | instr -> instr
+  in
+  List.map fixup_instr func
 
 (*
 let parsed_func = parse_function func true;;
