745243c99dd7c905c42010534217341ddbc3c53e
Samrat Man Singh <samratmansingh@gmail.com>
2017-06-04 18:39:00 +0545

    Update lexer tests

--- test_lexer.ml
+++ test_lexer.ml
@@ -4,14 +4,14 @@ open Qbe_lexer
 let test_lexer _ =
   let str = "function data %n1 =w phi @start %num, @loop1 %n2" in
   let stream = stream_of_string str in
-  assert_equal (next_token stream) (Keyword("function"));
-  assert_equal (next_token stream) (Keyword("data"));
-  assert_equal (next_token stream) (Ident("%n1"));
-  assert_equal (next_token stream) Equals;
-  assert_equal (next_token stream) (Ident("w"));
-  assert_equal (next_token stream) (Keyword("phi"));
-  assert_equal (next_token stream) (Ident("@start"));
-  assert_equal (next_token stream) (Ident("%num"));
-  assert_equal (next_token stream) Comma;
-  assert_equal (next_token stream) (Ident("@loop1"));
-  assert_equal (next_token stream) (Ident("%n2"))
+  assert_equal (next_token stream) (Some(Keyword("function")));
+  assert_equal (next_token stream) (Some(Keyword("data")));
+  assert_equal (next_token stream) (Some(Ident("%n1")));
+  assert_equal (next_token stream) (Some(Equals));
+  assert_equal (next_token stream) (Some(Ident("w")));
+  assert_equal (next_token stream) (Some(Keyword("phi")));
+  assert_equal (next_token stream) (Some(Ident("@start")));
+  assert_equal (next_token stream) (Some(Ident("%num")));
+  assert_equal (next_token stream) (Some(Comma));
+  assert_equal (next_token stream) (Some(Ident("@loop1")));
+  assert_equal (next_token stream) (Some(Ident("%n2")));
