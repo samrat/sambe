af0d7f738d78b20b2fa56424b941d552d76a34b1
Samrat Man Singh <samratmansingh@gmail.com>
2017-06-06 15:01:05 +0545

    Check-in lexer mli file

---
+++ src/qbe_lexer.mli
@@ -0,0 +1,19 @@
+type token = Ident of string
+           | Integer of int
+           | Float of float     (* TODO: f32 and f64 *)
+           | Double of float
+           | Keyword of string
+           | String of string
+           | LParen | RParen
+           | LBrace | RBrace
+           | Comma
+           | Pound
+           | Equals
+
+type lexer_state = { mutable line_num: int; mutable column: int;
+                     stm: char Stream.t; mutable next_token: token option }
+
+val peek_token : lexer_state -> token option
+val next_token : lexer_state -> token option
+val open_stream : string -> lexer_state
+val stream_of_string: string -> lexer_state
